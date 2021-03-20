fb751c724ec4f544bcd25a471f2cb66f6957f7d1
Dexler <69513582+DexlerXD@users.noreply.github.com>
2021-03-20 12:47:53 +0300

    Создание интерфейса
    Создание интерфейса + создание главного меню

---
+++ HistoryQuest/Assets/Scripts/AnswerSystem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerSystem : MonoBehaviour
+{
+    public void PickAnswer()
+    {
+        Debug.Log("Answer picked");
+
+    }
+}
---
+++ HistoryQuest/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Starting game session...");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Closing application!");
+        Application.Quit();
+    }
+}
