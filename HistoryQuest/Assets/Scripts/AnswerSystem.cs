8fef829cb336ca9aca3488c01513f54ff63a1966
Dexler <69513582+DexlerXD@users.noreply.github.com>
2021-04-20 17:11:25 +0300

    Добавление системы выбора ответ/вопрос

--- HistoryQuest/Assets/Scripts/AnswerSystem.cs
+++ HistoryQuest/Assets/Scripts/AnswerSystem.cs
@@ -1,12 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AnswerSystem : MonoBehaviour
 {
+
+    public bool isCorrect = false;
+    public GameManager GameManager;
+
     public void PickAnswer()
     {
-        Debug.Log("Answer picked");
+        if (isCorrect == true)
+        {
+            Debug.Log("Answer is correct");
+            GameManager.correct();
+
+        }
+        else if (isCorrect == false)
+        {
+            Debug.Log("Answer is wrong");
+        }
+        else
+        {
+            Debug.LogError("Something is wrong!");
+        }
 
     }
 }
--- HistoryQuest/Assets/Scripts/GameManager.cs
+++ HistoryQuest/Assets/Scripts/GameManager.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<QnA> QnA;
+    public GameObject[] options;
+    public int currentQuestion;
+    public TMP_Text qText;
+
+    private void Start()
     {
-        
+        QuestionGenerator();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void correct()
     {
-        
+        QnA.RemoveAt(currentQuestion);
+        QuestionGenerator();
+    }
+
+    void SetAnswer()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<AnswerSystem>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<TMP_Text>().text = QnA[currentQuestion].Answers[i];
+
+            if (QnA[currentQuestion].CorrectAnswer == i + 1)
+            {
+                options[i].GetComponent<AnswerSystem>().isCorrect = true;
+            }
+        }
+    }
+
+    void QuestionGenerator()
+    {
+        currentQuestion = Random.Range(0, QnA.Count);
+        qText.text = QnA[currentQuestion].Question;
+        SetAnswer();
     }
 }
