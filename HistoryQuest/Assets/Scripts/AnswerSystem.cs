040dfdbb7be569a5e66974f92cadea5d0c12ebdd
Dexler <69513582+DexlerXD@users.noreply.github.com>
2022-01-13 10:25:02 +0300

    AnswerSystem refactoring

--- HistoryQuest/Assets/Scripts/AnswerSystem.cs
+++ HistoryQuest/Assets/Scripts/AnswerSystem.cs
@@ -2,30 +2,20 @@
 
 public class AnswerSystem : MonoBehaviour
 {
-
-    public bool isCorrect = false;//По началу, переменная которая отвечает за правильность ответа неверна
-    public GameManager GameManager;//Переменная, которая представляет собой GameManager, для дальнейшего использования
+    public bool isCorrect = false;
+    public GameManager GameManager;
 
     public void PickAnswer()
     {
-        if (isCorrect == true)
-        {
-            Debug.Log("Answer is correct");
-            GameManager.score++;
-            GameManager.GameCycle();
-            //Если выбран правильный ответ, то мы добавляем к счетчику очков 1 и вызываем функцию GameCycle в GameManager
-        }
-        else if (isCorrect == false)
-        {
+        if (!isCorrect){
             Debug.Log("Answer is wrong");
             GameManager.GameCycle();
-            //Если выбран неправльиный ответ, то мы просто вызвываем функцию GameCycle
-        }
-        else
-        {
-            Debug.LogError("Something is wrong!");
-            //Если выбранный ответ каким-либо образом не выполняет ни одно из условий то мы выводим ошибку в консоли
+            return;
+            //Возврат в случае неправильного ответа
         }
 
+        Debug.Log("Answer is correct");
+        GameManager.score++;
+        GameManager.GameCycle();
     }
 }
